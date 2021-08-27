9d3d582ba2b6e19858ead369c536fc2275d46ff8
Allen <allenhe77@gmail.com>
2021-08-27 10:31:57 +0300

    some changes. You can look at it before the lesson.

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const express = require("express")
-const json = require("json")
-const app = express()
-app.use(express.json())
-const http = require("http")
+const express = require("express");
+const json = require("json");
+const app = express();
+app.use(express.json());
+const http = require("http");
+const path = require("path");
+let ejs = require("ejs");
 
-app.use("/home", express.static("index.html"))
+// app.use(express.static("index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/lobby", (req, res) => {
+  res.render(__dirname + "/lobby.ejs", { users: user });
+});
+app.post("/", (req, res) => {
+  if (user.includes(req.body.name)) {
+    res.json({
+      msg: "failed",
+    });
+  } else {
+    user.push(req.body.name);
+    console.log(user);
+    res.json({
+      msg: "success",
+    });
+  }
+});
 
+const server = http.createServer(app);
+server.listen(3000, () => console.log("Listening to port:3000"));
 
-app.post("/", (req,res)=>{user.push(req.body.name); console.log(user);})
-
-const server = http.createServer(app)
-server.listen(3000, ()=>console.log("Listening to port:3000"))
-
-var user = []
-
-
+var user = [];
